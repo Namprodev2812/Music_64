5433d5b53f010d8e2a70dd9178e9e3e12aee22b4
Namprodev2812 <mailakudoshinichi17@gmail.com>
2019-05-15 15:30:16 +0700

    create ui genres list

---
+++ app/src/main/java/com/sun/music_64/genrescreen/GenreActivity.java
@@ -0,0 +1,105 @@
+package com.sun.music_64.genrescreen;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.sun.music_64.R;
+import com.sun.music_64.data.model.Track;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenreActivity extends AppCompatActivity implements LoadContract.View, ItemClickRecyclerView {
+
+    private static final String KEY_PUT = "sendtogenre";
+    private List<Track> mTracks;
+    private static final int LIMIT = 30;
+    private static final int OFFSET = 0;
+    private Toolbar mToolbar;
+    private RecyclerView mRecyclerViewGenres;
+    private LinearLayoutManager mLinearLayoutManager;
+    private GenreAdapter mGenreAdapter;
+    private ItemClickRecyclerView mItemClickRecyclerView;
+    private LoadPresenter mLoadPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre);
+
+        initviews();
+        initToolbarGenreList();
+        initRecyclerViewGenres();
+        getData();
+    }
+
+    private void getData() {
+        initPresenter(getIntent().getStringExtra(KEY_PUT));
+    }
+
+    private void initviews() {
+        mToolbar = findViewById(R.id.toolbar_list);
+        mRecyclerViewGenres = findViewById(R.id.recycler_music);
+    }
+
+    private void initToolbarGenreList() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void initRecyclerViewGenres() {
+        mTracks = new ArrayList<>();
+        mItemClickRecyclerView = this;
+        mLinearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mGenreAdapter = new GenreAdapter(getApplicationContext(), mTracks, mItemClickRecyclerView);
+        mRecyclerViewGenres.setLayoutManager(mLinearLayoutManager);
+        mRecyclerViewGenres.setAdapter(mGenreAdapter);
+    }
+
+    private void initPresenter(String keygenre) {
+        mLoadPresenter = new LoadPresenter();
+        mLoadPresenter.setView(this);
+        mLoadPresenter.handleLoadGenre(keygenre, LIMIT, OFFSET);
+    }
+
+    private void cancelConnect() {
+        mLoadPresenter.cancelAsync();
+    }
+
+    @Override
+    public void loadUserSuccess(List<Track> tracks) {
+        mTracks.clear();
+        mTracks.addAll(tracks);
+        mGenreAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void loadUserFailure(String error) {
+        Toast.makeText(this, "Load fail", Toast.LENGTH_SHORT).show();
+        cancelConnect();
+    }
+
+    @Override
+    public void onClick(View view, int position) {
+        Toast.makeText(this, "" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onShow(View view, int position) {
+        Toast.makeText(this, "Hello show infor" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void gotoActivity(Context context, String keygenre) {
+        Intent intent = new Intent(context, GenreActivity.class);
+        intent.putExtra(KEY_PUT, keygenre);
+        context.startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/sun/music_64/genrescreen/GenreAdapter.java
@@ -0,0 +1,90 @@
+package com.sun.music_64.genrescreen;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.sun.music_64.R;
+import com.sun.music_64.data.model.Track;
+
+import java.util.List;
+
+public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Track> mTracks;
+    private ItemClickRecyclerView mItemClickRecyclerView;
+
+    public GenreAdapter(Context context, List<Track> tracks, ItemClickRecyclerView itemClickRecyclerView) {
+        mContext = context;
+        mTracks = tracks;
+        mItemClickRecyclerView = itemClickRecyclerView;
+    }
+
+    @NonNull
+    @Override
+    public GenreAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.genre_item_list, viewGroup, false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GenreAdapter.MyViewHolder viewHolder, int i) {
+        viewHolder.setOnItenClickListener(mItemClickRecyclerView);
+        viewHolder.bindData(i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTracks == null ? 0 : mTracks.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTextSongName;
+        public TextView mTextAuthName;
+        public TextView mTextPosition;
+        public ImageButton mImageShow;
+        public ItemClickRecyclerView mItemClickRecyclerView;
+
+        public MyViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            mTextSongName = itemView.findViewById(R.id.text_song_name);
+            mTextAuthName = itemView.findViewById(R.id.text_auth_name);
+            mTextPosition = itemView.findViewById(R.id.text_index);
+            mImageShow = itemView.findViewById(R.id.image_show_infor);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mItemClickRecyclerView != null) {
+                        mItemClickRecyclerView.onClick(itemView, getLayoutPosition());
+                    }
+                }
+            });
+            mImageShow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mItemClickRecyclerView != null) {
+                        mItemClickRecyclerView.onShow(itemView, getLayoutPosition());
+                    }
+                }
+            });
+        }
+
+        public void setOnItenClickListener(ItemClickRecyclerView itemClickListener) {
+            this.mItemClickRecyclerView = itemClickListener;
+        }
+
+        public void bindData(int position) {
+            mTextSongName.setText(mTracks.get(position).getTitle());
+            mTextPosition.setText(String.valueOf(position));
+            mTextAuthName.setText(mTracks.get(position).getArtist());
+        }
+    }
+}
